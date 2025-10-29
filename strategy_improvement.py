8f1a03d33e6db89aa8f4ad3325c13a15aebe82e0
sarv-hp <rezvanbahmani1997@gmail.com>
2025-10-29 17:43:48 +0000

    strategy improvement module added

--- strategy_improvement.py
+++ strategy_improvement.py
@@ -3,4 +3,12 @@ from automata import Automata
 from product import Product
 
 
+address = 'Ab_UAV_10-16-2025_20-48-14'
+noise_samples = 20000
+I = IMDP(address=address, noise_samples=noise_samples)
 
+all_labsets = {I.label[s] for s in I.states}
+B = Automata(all_labsets, "my_automaton.hoa")
+print('will build product')
+P = Product(I, B)
+print('product is build')
